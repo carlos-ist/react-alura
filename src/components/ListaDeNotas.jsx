f2691d83c7bd5aa1fc1ed3632a1404eeaa775ffe
carlos-ist <carlos.work@outlook.com>
2022-06-25 14:05:02 -0300

    Componentizando o sistema

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,36 +1,25 @@
 import React, {Component} from "react";
+import CardNota from "./CardNota";
+
 
 //Para importar sem usar as chaves, usar o export default
 // export default class...
-export class ListaDeNotas extends Component {
+class ListaDeNotas extends Component {
     render(){
         return (
             <ul>
                 <li>
-                <section>
-                    <header>
-                    <h3>Titulo</h3>
-                    </header>
-                    <p>Escreva sua nota</p>
-                </section>
+                    <CardNota/>
                 </li>
                 <li>
-                <section>
-                    <header>
-                    <h3>Titulo</h3>
-                    </header>
-                    <p>Escreva sua nota</p>
-                </section>
+                    <CardNota/>
                 </li>
                 <li>
-                <section>
-                    <header>
-                    <h3>Titulo</h3>
-                    </header>
-                    <p>Escreva sua nota</p>
-                </section>
+                    <CardNota/>
                 </li>
              </ul>
         )
     }
 }
+
+export default ListaDeNotas;
